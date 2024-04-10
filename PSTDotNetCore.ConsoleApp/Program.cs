4f7ba179c75cbf9c125a59d27349eeaeab44f8fc
phyusindev <devphyusin@gmail.com>
2024-04-10 10:37:08 +0630

    .

--- PSTDotNetCore.ConsoleApp/Program.cs
+++ PSTDotNetCore.ConsoleApp/Program.cs
@@ -1,1 +1,39 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = ".";
+stringBuilder.InitialCatalog = "DotNetTrainingBatch4";
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "sa@123";
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+
+connection.Open();
+Console.WriteLine("Connection open.");
+
+string query = "Select * from Tbl_Blog";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);
+
+connection.Close();
+Console.WriteLine("Connection close.");
+
+//can retrieve after closing con
+//dataset => multi datatables
+//datatable => datarow
+//datarow => datacolumn
+
+foreach(DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog Id => " + dr["BlogId"]);
+    Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+    Console.WriteLine("Blog Author =>" + dr["BlogAuthor"]);
+    Console.WriteLine("Blog Content =>" + dr["BlogContent"]);
+    Console.WriteLine("------------------------------");
+}
+
+Console.ReadKey();
