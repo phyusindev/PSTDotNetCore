3bec706d5cd6787fed3ed5f87fdfdc645d446056
phyusindev <devphyusin@gmail.com>
2024-05-03 13:13:24 +0630

    [InProgress] Ado.net Custome Service

---
+++ PSTDotNetCore.RestApi/Controllers/BlogAdoDotNet2Controller.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PSTDotNetCore.RestApi.Models;
+using System.Data;
+using System.Data.SqlClient;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using PSTDotNetCore.Shared;
+
+namespace PSTDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] 
+    public class BlogAdoDotNet2Controller : ControllerBase
+    {
+        private readonly AdoDotNetService _adoDotNetService = new AdoDotNetService(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            string query = "select * from tbl_blog";
+            var lst = _adoDotNetService.Query<BlogModel>(query);
+
+            return Ok(lst);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlog(int id)
+        {
+            string query = "Select * from Tbl_Blog where BlogId = @BlogId";
+
+
+            var item = _adoDotNetService.QueryFirstOrDefault<BlogModel>(query,
+                new AdoDotNetParameter("@BlogId", id));
+
+            if (item is null)
+            {
+                return NotFound("No Data Found.");
+            }
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(BlogModel blog)
+        {
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent)";
+
+ 
+            int result = _adoDotNetService.Execute(query,
+                new AdoDotNetParameter("@BlogTitle", blog.BlogTitle),
+                new AdoDotNetParameter("@BlogAuthor", blog.BlogAuthor),
+                new AdoDotNetParameter("@BlogContent", blog.BlogContent));
+
+        
+            string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+            return Ok(message);
+        }
+
+       
+
+    }
+}
---
+++ PSTDotNetCore.Shared/AdoDotNetService.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace PSTDotNetCore.Shared
+{
+    public class AdoDotNetService
+    {
+        private readonly string _connectionString;
+
+        public AdoDotNetService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<T> Query<T>(string query, params AdoDotNetParameter[]? parameters) 
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();  
+
+            
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            if(parameters is not null && parameters.Length > 0)
+            {
+                var parametersArray = parameters.Select(p => new SqlParameter(p.Name, p.Value)).ToArray();
+                cmd.Parameters.AddRange(parametersArray);
+            }
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+
+
+            string json = JsonConvert.SerializeObject(dt);
+            List<T> lst = JsonConvert.DeserializeObject<List<T>>(json)!;
+
+            return lst;
+        }
+        public T QueryFirstOrDefault<T>(string query, params AdoDotNetParameter[]? parameters)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            if (parameters is not null && parameters.Length > 0)
+            {
+                var parametersArray = parameters.Select(p => new SqlParameter(p.Name, p.Value)).ToArray();
+                cmd.Parameters.AddRange(parametersArray);
+            }
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+
+            if (dt.Rows.Count == 0)
+            {
+                return default;
+            }
+
+            connection.Close();
+
+
+            string json = JsonConvert.SerializeObject(dt); //C# to json
+            List<T> lst = JsonConvert.DeserializeObject<List<T>>(json)!;  //json to c#
+
+            return lst[0];
+        }
+
+        public int Execute(string query, params AdoDotNetParameter[]? parameters)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            if (parameters is not null && parameters.Length > 0)
+            {
+                cmd.Parameters.AddRange(parameters.Select(p => new SqlParameter(p.Name, p.Value)).ToArray());
+            }
+
+            var result = cmd.ExecuteNonQuery();
+
+
+            connection.Close();
+            return result;
+        }
+    }
+    public class AdoDotNetParameter
+    {
+        public AdoDotNetParameter()
+        {
+
+        }
+        public AdoDotNetParameter(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+        public string Name { get; set; }
+        public object Value { get; set; }
+
+        
+    }
+}
