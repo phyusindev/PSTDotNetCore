82dfb3c2ffb94650274529cbbfbd79eee9570df2
phyusindev <devphyusin@gmail.com>
2024-05-05 13:40:15 +0630

    [Complete] Ado.net Custom Service CRUD

--- PSTDotNetCore.RestApi/Controllers/BlogAdoDotNet2Controller.cs
+++ PSTDotNetCore.RestApi/Controllers/BlogAdoDotNet2Controller.cs
@@ -5,11 +5,12 @@ using System.Data;
 using System.Data.SqlClient;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 using PSTDotNetCore.Shared;
+using System.Reflection.Metadata;
 
 namespace PSTDotNetCore.RestApi.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController] 
+    [ApiController]
     public class BlogAdoDotNet2Controller : ControllerBase
     {
         private readonly AdoDotNetService _adoDotNetService = new AdoDotNetService(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
@@ -53,18 +54,119 @@ namespace PSTDotNetCore.RestApi.Controllers
            ,@BlogAuthor
            ,@BlogContent)";
 
- 
+
             int result = _adoDotNetService.Execute(query,
                 new AdoDotNetParameter("@BlogTitle", blog.BlogTitle),
                 new AdoDotNetParameter("@BlogAuthor", blog.BlogAuthor),
                 new AdoDotNetParameter("@BlogContent", blog.BlogContent));
 
-        
+
             string message = result > 0 ? "Saving Successful." : "Saving Failed.";
             return Ok(message);
         }
 
-       
+        [HttpPut("{id}")]
+        public IActionResult PutBlog(int id,BlogModel blog)
+        {
+            var item = FindById(id);
+            if (item == null)
+            {
+                return NotFound("No Data Found.");
+            }
+
+            string query = @"UPDATE [dbo].[Tbl_Blog]
+       SET [BlogTitle] = @BlogTitle
+          ,[BlogAuthor] = @BlogAuthor
+          ,[BlogContent] = @BlogContent
+     WHERE BlogId = @BlogId";
+
+            int result = _adoDotNetService.Execute(query,
+                new AdoDotNetParameter("@BlogId", id),
+                new AdoDotNetParameter("@BlogTitle", blog.BlogTitle),
+                new AdoDotNetParameter("@BlogAuthor", blog.BlogAuthor),
+                new AdoDotNetParameter("@BlogContent", blog.BlogContent));
+
+            string message = result > 0 ? "Updating Successful." : "Updating Failed.";
+            return Ok(message);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult PatchBlog(int id, BlogModel blog)
+        {
+            var item = FindById(id);
+            if (item == null)
+            {
+                return NotFound("No Data Found.");
+            }
+
+            string condition = string.Empty;
+            if (!string.IsNullOrEmpty(blog.BlogTitle))
+            {
+                condition += "[BlogTitle] = @BlogTitle, ";
+            }
+            if (!string.IsNullOrEmpty(blog.BlogAuthor))
+            {
+                condition += "[BlogAuthor] = @BlogAuthor, ";
+            }
+            if (!string.IsNullOrEmpty(blog.BlogContent))
+            {
+                condition += "[BlogContent] = @BlogContent, ";
+            }
+            condition = condition.Substring(0, condition.Length - 2);
+            if (condition.Length == 0)
+            {
+                return NotFound("No data to update.");
+            }
+
+            string query = $@"UPDATE [dbo].[Tbl_Blog]
+       SET {condition}
+     WHERE BlogId = @BlogId";
+                        
+            int result = _adoDotNetService.Execute(query,
+                new AdoDotNetParameter("@BlogId", id),
+                new AdoDotNetParameter("@BlogTitle", blog.BlogTitle ?? (object)DBNull.Value),
+            new AdoDotNetParameter("@BlogAuthor", blog.BlogAuthor ?? (object)DBNull.Value),
+            new AdoDotNetParameter("@BlogContent", blog.BlogContent ?? (object)DBNull.Value));
+
+            string message = result > 0 ? "Updating Successful." : "Updating Failed.";
+            return Ok(message);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlog(int id)
+        {
+            var item=FindById(id);
+            if (item == null)
+            {
+                return NotFound("No Data Found.");
+            }
+
+            string query = "DELETE FROM[dbo].[Tbl_Blog] WHERE BlogId = @BlogId";
+
+            int result = _adoDotNetService.Execute(query,
+                new AdoDotNetParameter("@BlogId", id));
+
+
+            string message = result > 0 ? "Deleting Successful." : "Deleting Failed.";
+            return Ok(message);
+        }
+
+        private BlogModel FindById(int id)
+        {
+            string query = "Select * from Tbl_Blog where BlogId = @BlogId";
+
+
+            var item = _adoDotNetService.QueryFirstOrDefault<BlogModel>(query,
+                new AdoDotNetParameter("@BlogId", id));
+
+            if (item is null)
+            {
+                return null;
+            }
+
+            return item;
+        }
+
 
     }
 }
