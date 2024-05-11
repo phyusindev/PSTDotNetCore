aba7143ae3964ba98a3130c40e6c1ca43bdf511c
phyusindev <devphyusin@gmail.com>
2024-05-11 18:49:34 +0630

    [Complete] HttpClientExample

---
+++ PSTDotNetCore.ConsoleAppHttpClientExample/HttpClientExample.cs
@@ -0,0 +1,138 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PSTDotNetCore.ConsoleAppHttpClientExample
+{
+    internal class HttpClientExample
+    {
+        private readonly HttpClient _client = new HttpClient() { BaseAddress = new Uri("https://localhost:7194") };
+        private readonly string _blogEndpoint = "api/blog";
+        public async Task RunAsync()
+        {
+            //await ReadAsync();
+            //await EditAsync(1);
+            //await EditAsync(200);
+            //await CreateAsync("title", "author 2", "content 2");
+            //await EditAsync(2015);
+            //await UpdateAsync(2015,"title2015", "author 2015", "content 2015");
+            //await PatchAsync(2016,"title2016","","");
+            //await PatchAsync(2017, "null", "author 2017", null);
+            //await EditAsync(2017);
+            await DeleteAsync(2015);
+        }
+
+        private async Task ReadAsync()
+        {
+            var response = await _client.GetAsync(_blogEndpoint);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                List<BlogDto> lst = JsonConvert.DeserializeObject<List<BlogDto>>(jsonStr)!;
+                foreach (var blog in lst)
+                {
+                    Console.WriteLine(JsonConvert.SerializeObject(blog));
+                    Console.WriteLine($"Title => {blog.BlogTitle}");
+                    Console.WriteLine($"Author => {blog.BlogAuthor}");
+                    Console.WriteLine($"Content => {blog.BlogContent}");
+                }
+            }
+        }
+        private async Task EditAsync(int id)
+        {
+            var response = await _client.GetAsync($"{_blogEndpoint}/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                var item = JsonConvert.DeserializeObject<BlogDto>(jsonStr)!;
+                Console.WriteLine(JsonConvert.SerializeObject(item));
+                Console.WriteLine($"Title => {item.BlogTitle}");
+                Console.WriteLine($"Author => {item.BlogAuthor}");
+                Console.WriteLine($"Content => {item.BlogContent}");
+            }
+            else
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+        }
+        private async Task CreateAsync(string title, string author, string content)
+        {
+            BlogDto blogDto = new BlogDto()
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };//c# to object
+
+            //To JSON
+            string blogJson = JsonConvert.SerializeObject(blogDto);
+
+            HttpContent httpContent = new StringContent(blogJson, Encoding.UTF8, Application.Json);
+            var response = await _client.PostAsync(_blogEndpoint,httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+        }
+        private async Task UpdateAsync(int id, string title, string author, string content)
+        {
+            BlogDto blogDto = new BlogDto()
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };//c# to object
+
+            //To JSON
+            string blogJson = JsonConvert.SerializeObject(blogDto);
+
+            HttpContent httpContent = new StringContent(blogJson, Encoding.UTF8, Application.Json);
+            var response = await _client.PutAsync($"{_blogEndpoint}/{id}", httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+        }
+        private async Task PatchAsync(int id, string? title, string? author, string? content)
+        {
+            BlogDto blogDto = new BlogDto()
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };//c# to object
+
+            //To JSON
+            string blogJson = JsonConvert.SerializeObject(blogDto);
+
+            HttpContent httpContent = new StringContent(blogJson, Encoding.UTF8, Application.Json);
+            var response = await _client.PatchAsync($"{_blogEndpoint}/{id}", httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+        }
+        private async Task DeleteAsync(int id)
+        {
+            var response = await _client.DeleteAsync($"{_blogEndpoint}/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+            else
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
---
+++ PSTDotNetCore.ConsoleAppHttpClientExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using PSTDotNetCore.ConsoleAppHttpClientExample;
+
+Console.WriteLine("Hello, World!");
+
+//Console App - Client (frontend)
+//ASP.NET Core Web API - Server (backend)
+
+
+HttpClientExample httpClientExample = new HttpClientExample();
+await httpClientExample.RunAsync();
+
+Console.ReadLine();
