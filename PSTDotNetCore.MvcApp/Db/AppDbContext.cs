a66698e4bca40b2dc5d044a95ab3d5c7884e7242
phyusindev <devphyusin@gmail.com>
2024-06-20 14:52:50 +0630

    [Complete] Mvc App BlogList

---
+++ PSTDotNetCore.MvcApp/Db/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PSTDotNetCore.MvcApp.Models;
+
+namespace PSTDotNetCore.MvcApp.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+        //}
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
---
+++ PSTDotNetCore.MvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PSTDotNetCore.MvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
